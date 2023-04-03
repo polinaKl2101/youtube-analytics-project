a434bc25daf8443dbef562de40dd3c16aa3c122c
PolinaK <pklopunova@gmail.com>
2023-04-03 13:14:22 +0600

    homework-6

--- src/video.py
+++ src/video.py
@@ -2,31 +2,43 @@ from googleapiclient.discovery import build
 import isodate
 import json
 import os
+from googleapiclient.errors import HttpError
 
 
 class Video:
-
     __api_key: str = os.getenv('YT_API_KEY')
     __youtube = build('youtube', 'v3', developerKey=__api_key)
 
     def __init__(self, video_id: str):
-        self.__video_id = video_id
-        self.__video_response = self.__youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                             id=self.__video_id).execute()
-        self.__video_title = self.__video_response['items'][0]['snippet']['title']
-        self.__video_link =f"https://www.youtube.com/watch?v={self.__video_id}"
-        self.__view_count: int = self.__video_response['items'][0]['statistics']['viewCount']
-        self.__like_count: int = self.__video_response['items'][0]['statistics']['likeCount']
+
+        try:
+            self.__video_id = video_id
+            self.__video_response = self.__youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                                 id=self.__video_id).execute()
+            self.__video_title = self.__video_response['items'][0]['snippet']['title']
+            self.__video_link = f"https://www.youtube.com/watch?v={self.__video_id}"
+            self.__view_count: int = self.__video_response['items'][0]['statistics']['viewCount']
+            self.__like_count: int = self.__video_response['items'][0]['statistics']['likeCount']
+
+        except:
+            self.__video_title = None
+            self.__video_link = None
+            self.__view_count = None
+            self.__like_count = None
 
     def __str__(self):
         return self.__video_title
 
+    def __check_id(self, video_id):
+        video_response = self.__youtube.videos().list(part='status', id=video_id).execute()
+        result = video_response['pageInfo']['totalResults']
+
     @property
-    def  video_id(self):
+    def video_id(self):
         return self.__video_id
 
     @property
-    def video_title(self):
+    def title(self):
         return self.__video_title
 
     @property
@@ -47,7 +59,6 @@ class PLVideo:
     __youtube = build('youtube', 'v3', developerKey=__api_key)
 
     def __init__(self, video_id, playlist_id):
-
         self.__video_id = video_id
         self.__video_response = self.__youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                              id=self.__video_id).execute()
@@ -61,7 +72,7 @@ class PLVideo:
         return self.__video_title
 
     @property
-    def  video_id(self):
+    def video_id(self):
         return self.__video_id
 
     @property
@@ -83,7 +94,3 @@ class PLVideo:
     @property
     def playlist_id(self):
         return self.__playlist_id
-
-
-
-
